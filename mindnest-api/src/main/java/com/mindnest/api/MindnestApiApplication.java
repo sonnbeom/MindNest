17c3c4a5358e9c9ee715049b241d9ea1fff8d3cb
sonnbeom <dhsmf4245@gmail.com>
2026-04-02 06:59:20 +0900

    Initial commit

---
+++ mindnest-api/src/main/java/com/mindnest/api/MindnestApiApplication.java
@@ -0,0 +1,13 @@
+package com.mindnest.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindnestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindnestApiApplication.class, args);
+	}
+
+}
---
+++ mindnest-api/src/test/java/com/mindnest/api/MindnestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindnest.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindnestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
